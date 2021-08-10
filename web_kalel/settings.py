cd40459a5d1d6baf5456e066d499471c7ffde38b
Miguel Vargas <mvargasp@hdec.com.pe>
2021-08-09 21:15:48 -0500

    public media

--- web_kalel/settings.py
+++ web_kalel/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/home/h43779c/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/h43779c/public_media')
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
