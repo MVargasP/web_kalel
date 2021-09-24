08aa7bd5e395290e8d5bd758b6f6b04a61207a79
Miguel Vargas <mvargasp@hdec.com.pe>
2021-09-24 10:50:17 -0500

    zip

--- generarqr/urls.py
+++ generarqr/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,re_path
-from .views import generate_qrcode
+from .views import generate_qrcode,generate_qrcode_empresa
 
 urlpatterns = [
-    path('qrcode/',generate_qrcode)
+    path('qrcode/',generate_qrcode),
+    path('qr_empresa/',generate_qrcode_empresa)
 ]
--- generarqr/views.py
+++ generarqr/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render,redirect, get_object_or_404 
 from django.views.generic import ListView, DetailView, View,TemplateView
 from apps.web.models import Contacto
+import os
+import shutil, sys 
+import qrcode
+img = qrcode.make("Hola desde Recursos Python!")
+f = open("output.png", "wb")
+img.save(f)
+f.close()
 
+def generate_qrcode_empresa(request):
+    if request.method == "POST":
+        buscar = request.POST['dni']
+        #print(os.listdir('static/empresa/CRISTO VIENE/'))
+        contenido = os.listdir('static/empresa/{}/TARJETA/'.format(buscar))
+        for x in contenido:
+            f = open("static/empresa/{}/QR/{}.png".format(buscar,x), "wb")
+            img.save(f)
+            f.close()
 
-"""class generate_qrcode(ListView):
-    model=Contacto
-   
-    def post(self,request,*args,**kwargs):
-        buscar= request.POST['dni']
-       
-        if buscar:
-            print(buscar)
-
-    
+        archivo_zip = shutil.make_archive("static/empresa/{}/comprimido/{}".format(buscar,buscar), 
+                                  "zip", 
+                                  "static/empresa/{}/QR".format(buscar))
+        ruta="static/empresa/{}/comprimido/{}".format(buscar,buscar)
         contexto = {
-            'fecha_actual':now,
-            'dni':placa,
-    
-            'busqueda':buscar,
+                'ruta':ruta }
+        return render(request,'codigoQr/qr_empresa.html',contexto)
 
-        }
-        return render(request,'codigoQr/codigos.html',contexto)
+    else:
 
-   
-"""
+        return render(request,'codigoQr/qr_empresa.html')
+
 
 
 def generate_qrcode(request):
